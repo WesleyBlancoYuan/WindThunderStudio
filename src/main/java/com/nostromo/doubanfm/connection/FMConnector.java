dc672c8faab6424d898f30a116666e91b56541d3
Wesley.Yuan <torresdyl@gmail.com>
2017-07-20 16:43:36 +0200

    first commit to WTS with branch name doubanfm

---
+++ src/main/java/com/nostromo/doubanfm/connection/FMConnector.java
@@ -0,0 +1,31 @@
+package com.nostromo.doubanfm.connection;
+
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
+import com.google.gson.Gson;
+
+public class FMConnector {
+    
+    public static final Logger logger = Logger.getLogger("com.nostromo.doubanfm.connection.FMConnector");
+    
+     
+    public FMConnector() {
+        
+    }
+    
+    public Gson autenticateUser() {
+        logger.entering(getClass().toString(), "autenticateUser");
+        Gson gson = new Gson();
+        try {
+            
+        } catch (Exception e) {
+            
+        } finally {
+            
+        }
+        logger.exiting(getClass().toString(), "autenticateUser");
+        return gson;
+    }
+    
+}
